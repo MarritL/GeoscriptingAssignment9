42ad82a452e3cfb32a63481f56c530621845b3b3
Marrit <marrit.leenstra@wur.nl>
2019-01-17 10:17:25 +0100

    data loaded and sample points works, some object names changed

--- main.R
+++ main.R
@@ -5,41 +5,40 @@
 # This project analyses how well landsat band reflectance can predict VCF tree cover.
 
 # load libraries
-if (!require("biglm")) install.packages("sp") 
 if (!require("raster")) install.packages("raster")
-library(biglm)
 library(raster)
 
 # source functions
-source("R/....R")
+source("R/retrieveData.R")
 
 # download and unzip data 
-retrieveData("...", "data")
+retrieveData("https://github.com/GeoScripting-WUR/AdvancedRasterAnalysis/raw/gh-pages/data/GewataBands.zip", "data")
 
 # load data to global environment
-load("data/GewataB1.rda")
-load("data/GewataB5.rda")
-load("data/GewataB7.rda")
-## Band 6 (thermal infra-red) will be excluded from this exercise
-## Build a brick containing all data
-alldata <- brick(GewataB1, GewataB2, GewataB3, GewataB4, GewataB5, GewataB7, vcfGewata)
-names(alldata) <- c("band1", "band2", "band3", "band4", "band5", "band7", "VCF")
+fileList <- list.files(path = "data/", pattern = glob2rx('*.rda'), full.names = TRUE)
+for (file in fileList){
+  load(file)
+}
 
-## Find borders of Gewate region
+# Build a brick containing all data
+gewata <- brick(GewataB1, GewataB2, GewataB3, GewataB4, GewataB5, GewataB7, vcfGewata)
+names(gewata) <- c("band1", "band2", "band3", "band4", "band5", "band7", "VCF")
+
+# Find borders of Gewate region
 eth <- getData('GADM', country='ETH', level=3)
 gwt <- eth[eth$NAME_3=="Getawa",]
 gwt <- spTransform(gwt, CRSobj = crs(vcfGewata))
 
 #apply the mask to all data
-alldata <- mask(x = alldata, mask=gwt)
-plotRGB(alldata, 3, 2, 1, stretch="lin", main = "RGB image of the Gewata region")
+gewata <- mask(x = gewata, mask=gwt)
+plotRGB(gewata, 3, 2, 1, stretch="lin", main = "RGB image of the Gewata region")
 
 ## Extract all data to a data.frame
-df <- as.data.frame(getValues(alldata))
+df <- as.data.frame(getValues(gewata))
 
 ## select random sample points (choose a size what you think is proper)
 ## and use as training data
-sRandomGWT <- sampleRandom(alldata, na.rm=TRUE, sp=TRUE, size = 1000)
+sRandomGWT <- sampleRandom(gewata, na.rm=TRUE, sp=TRUE, size = 1000)
 sRandomData <- data.frame(VCF = sRandomGWT@data[[7]], 
                           band1 = sRandomGWT@data[[1]],
                           band2 = sRandomGWT@data[[2]],
